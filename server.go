7fc0a972b851fdedc2f380e13fd49adb7a996977
mammenj <mammenj@gmail.com>
2017-08-12 12:08:58 +0530

    added lissajous function

--- server.go
+++ server.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 	"sync"
 )
 
 var mu sync.Mutex
 var count int
+var pallete = []color.Color{color.White, color.Black}
 
 func main() {
 	http.HandleFunc("/", handleFunc)
 	http.HandleFunc("/count", counter)
+	http.HandleFunc("/l", lissFunc)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
-
+func lissFunc(w http.ResponseWriter, r *http.Request) {
+	lissajous(w)
+}
 func handleFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
 	for k, v := range r.Header {
@@ -41,4 +51,34 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Count = %d\n", count)
 	mu.Unlock()
 
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles     = 5
+		res        = 0.001
+		size       = 100
+		nframes    = 64
+		delay      = 8
+		blackIndex = 1
+		whiteIndex = 0
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, pallete)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+
+	}
+	gif.EncodeAll(out, &anim)
 }
